2b91b16a5a34b6204ea7d03ead19cf194decff1e
Tiyyo <111092370+Tiyyo@users.noreply.github.com>
2022-09-23 09:03:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,33 +3,34 @@ const slideBar = document.getElementsByClassName("slidebar");
 const pageview = document.getElementById("numberPages");
 const price = document.getElementsByClassName("subscription__price--dollars");
 const displayDiscount = document.getElementById("switch");
-let rangeValue;
-let priceMonth;
-let priceYear;
-const body = document.querySelector("body");
+const priceFreq = document.getElementsByClassName("monthly");
 const tSwitch = document.querySelector(".round");
+let rangeValue;
 let pageviews = ["10k", "50K", "100K", "500K", "1M"];
 let perMonth = ["$8.00", "$12.00", "$16.00", "$24.00", "$36.00"];
 let perYear = ["$6,00", "$8,00", "$12,00", "$18,00", "$27,00"];
 let i;
-let toggleEnable;
+var toggleEnable;
+
+// return true is toggle is enable
+function isAnnualFrequency() {
+  return displayDiscount.checked;
+}
 
 // function which display prices per month and number of pages chosen
 function displayPriceandPages(i) {
-  if (rangeValue === i) {
+  const isAnnual = isAnnualFrequency();
+  console.log(isAnnual);
+  if (rangeValue === i && isAnnual === false) {
     price[0].textContent = perMonth[i] + " ";
+    pageview.textContent = pageviews[i] + " ";
+  } else {
+    price[0].textContent = perYear[i] + " ";
     pageview.textContent = pageviews[i] + " ";
   }
 }
 
-// function which dispay prices after discount
-function displayYearlyPrice(i) {
-  if (rangeValue === i) {
-    price[0].textContent = perYear[i] + " ";
-  }
-}
-
-// display number/pagesview and prices/month
+// display number/pageview and prices/month
 rangeInput.range.addEventListener("input", (e) => {
   rangeValue = e.target.value;
   rangeBackground = e.target.value * 25;
@@ -41,20 +42,26 @@ rangeInput.range.addEventListener("input", (e) => {
     hsl(174, 77%, 80%) ${rangeBackground}%, 
     hsl(224, 65%, 95%) 0%,
     hsl(224, 65%, 95%) 100%)`;
-  if (toggleEnable === false) {
-    displayPriceandPages(rangeValue);
-  } else displayYearlyPrice(rangeValue);
+
+  displayPriceandPages(rangeValue);
 });
 
 // display price/year
 
-displayDiscount.addEventListener("change", (e) => {
-  let toggleEnable = e.composedPath()[0].checked;
-  if (toggleEnable === true) {
-    console.log(rangeValue);
-    displayYearlyPrice(rangeValue);
-    tSwitch.classList.add("active");
-  } else {
-    tSwitch.classList.remove("active");
-  }
-});
+function displayToggleBackground() {
+  displayDiscount.addEventListener("input", (e) => {
+    var toggleEnable = e.composedPath()[0].checked;
+    console.log(price[0].textContent);
+    if (price[0].textContent === undefined) {
+      price[0].textContent = "$0.00";
+    }
+    if (toggleEnable === true) {
+      tSwitch.classList.add("active");
+      displayPriceandPages(rangeValue);
+    } else {
+      tSwitch.classList.remove("active");
+      displayPriceandPages(rangeValue);
+    }
+  });
+}
+displayToggleBackground();
--- trial.js
+++ trial.js
@@ -1,62 +1,71 @@
-const rangeInput = document.getElementsByClassName("slidebar__input");
-const pageview = document.getElementById("numberPages");
-const price = document.getElementsByClassName("subscription__price--dollars");
+
+
+
+const rangeInput = document.getElementsByClassName("slidebar__input").range; //rangeInput
+const slideBar = document.getElementsByClassName("slidebar");
+const pageview = document.getElementById("numberPages"); // viewcontainer
+const price = document.getElementsByClassName("subscription__price--dollars"); //costcontainer
+const priceFreq = document.getElementsByClassName("monthly");
 const displayDiscount = document.getElementById("switch");
-let rangeValue;
-let priceMonth;
-let priceYear;
-const body = document.querySelector("body");
-const tSwitch = document.querySelector(".round");
 
-console.log(displayDiscount.checked);
+const DATA = [
+  {
+    pageviews: "10K",
+    monthlycost: 8,
+  },
+  {
+    pageviews: "50K",
+    monthlycost: 12,
+  },
+  {
+    pageviews: "100K",
+    monthlycost: 16,
+  },
+  {
+    pageviews: "500K",
+    monthlycost: 24,
+  },
+  {
+    pageviews: "1M",
+    monthlycost: 36,
+  },
+];
 
-// console.log(price[0]);
+function getData() {
+  const currentValue = rangeInput.value;
+  const index = currentValue;
+  return ({ pageviews, monthlycost } = DATA[index]);
+}
 
-// display number/pagesview and prices/month
-rangeInput.range.addEventListener("input", (e) => {
-  rangeValue = e.target.value / 100;
-  rangeBackground = e.target.value;
+function displayPage() {
+  const { pageviews } = getData();
+  pageview.textContent = `${pageviews} `;
+}
 
-  rangeInput[0].style.background = `linear-gradient(90deg, 
+function isAnnualFrequency() {
+  return frequencyToggle.checked;
+}
+
+function displayPrice() {
+  const { monthlycost } = getData();
+  const isAnnual = isAnnualFrequency();
+  const cost = isAnnual ? monthlycost * 0.75 : monthlycost;
+  price[0].textContent = `$${cost.toFixed(2)}`;
+  if (isAnnual) {priceFreq.textContent ='/month'} else {
+    priceFreq.textContent ='/year'
+  }
+}
+
+function displayPriceAndPage() {
+  displayPage();
+  displayPrice();
+}
+
+rangeInput.addEventListener("input", 
+rangeInput[0].style.background = `linear-gradient(90deg, 
     hsl(174, 77%, 80%) 0%,
     hsl(174, 77%, 80%) ${rangeBackground}%, 
     hsl(224, 65%, 95%) 0%,
     hsl(224, 65%, 95%) 100%)`;
-
-  if (rangeValue <= 0.25) {
-    pageview.textContent = "10K ";
-    price[0].textContent = "$8.00 ";
-  }
-  if (displayDiscount === true) {
-    price[0].textContent = "$6.00";
-  }
-  if (rangeValue <= 0.5 && rangeValue > 0.25) {
-    pageview.textContent = "50K ";
-    price[0].textContent = "$16.00 ";
-  }
-  if (rangeValue <= 0.75 && rangeValue > 0.5) {
-    pageview.textContent = "500K ";
-    price[0].textContent = "$24.00 ";
-  }
-  if (rangeValue <= 1 && rangeValue > 0.75) {
-    pageview.textContent = "1M ";
-    price[0].textContent = "$36.00 ";
-  }
-});
-
-// display price/year
-displayDiscount.addEventListener("input", (e) => {
-  let toggleEnable = e.composedPath()[0].checked;
-
-  if (toggleEnable === true) {
-    let priceMonth = price[0].textContent.split("$")[1];
-    let priceYear = parseInt(priceMonth) - (25 * parseInt(priceMonth)) / 100;
-
-    price[0].textContent = "$" + priceYear + ".00";
-    tSwitch.classList.add("active");
-  } else {
-    let priceMonth = price[0].textContent.split("$")[1];
-    price[0].textContent = "$" + priceMonth;
-    tSwitch.classList.remove("active");
-  }
-});
+    displayPriceAndPage()
+    );
